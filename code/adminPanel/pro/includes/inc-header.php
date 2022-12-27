f39b893331f707da4bc774f90fb342df6bd5955b
Mizbaul Haque Maruf <mizbaulhaque@iut-dhaka.edu>
2022-12-27 21:05:56 +0600

    admin registration added

--- code/adminPanel/pro/includes/inc-header.php
+++ code/adminPanel/pro/includes/inc-header.php
@@ -6,7 +6,7 @@ require_once '../constants.php';
 ?>
 
 <head>
-    <title><?php echo SITE_NAME ?></title>
+    <title><?php echo RAIL-TICKETS.COM ?></title>
     <link href="assets/css/jquery-ui.css" rel="stylesheet" type="text/csss" />
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="assets/css/style.css">
