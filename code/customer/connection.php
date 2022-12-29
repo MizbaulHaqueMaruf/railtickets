f7b8f07c992afed04f79b557a7a5a08eb1b1aa0a
Mizbaul Haque Maruf <mizbaulhaque@iut-dhaka.edu>
2022-12-30 00:58:07 +0600

    updated conn

--- code/customer/connection.php
+++ code/customer/connection.php
@@ -3,7 +3,7 @@ $host="localhost"; // Host name
 $username="root"; // Mysql username
 $password=""; // Mysql password
 
-$db_name="finalrailres_6"; // Database name
+$db_name="finalrailres_7"; // Database name
 
 $conn=mysqli_connect("$host", "$username", "$password","$db_name")or die("cannot connect");
 ?>
